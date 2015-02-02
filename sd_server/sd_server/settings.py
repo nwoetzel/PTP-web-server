ee176dc2735006c95654ef502cb1b8dea1f4c5fd
woetzens <nils.woetzel@h-its.org>
2015-02-02 17:49:08 +0100

    added missing '/' for sqlite.db database file

--- sd_server/sd_server/settings.py
+++ sd_server/sd_server/settings.py
@@ -28,7 +28,7 @@ class Dev(Base):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': PROJECT_DIR + 'sqlite.db', # Or path to database file if using sqlite3.
+            'NAME': PROJECT_DIR + '/sqlite.db', # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': '',
             'PASSWORD': '',
