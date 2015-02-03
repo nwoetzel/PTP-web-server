83c081c8ce09a7759b951effb1750b7e98c9d9a4
woetzens <nils.woetzel@h-its.org>
2015-02-03 17:04:10 +0100

    added R requirements

---
+++ sd_server/requirements.R
@@ -0,0 +1,3 @@
+install.packages("ape",repos="http://cran.r-project.org/",type="source",dependencies=TRUE,verbose=TRUE)
+install.packages("paran",repos="http://cran.r-project.org/",type="source",dependencies=TRUE,verbose=TRUE)
+install.packages("splits",repos="http://R-Forge.R-project.org",type="source",dependencies=TRUE,verbose=TRUE)
