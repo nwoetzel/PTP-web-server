26aac94e8f26e08244a7fdd680e75a89e6ceeed3
woetzens <nils.woetzel@h-its.org>
2015-02-04 10:53:42 +0100

    modified wsgi.py to work with django_configurations

--- sd_server/sd_server/wsgi.py
+++ sd_server/sd_server/wsgi.py
@@ -20,13 +20,10 @@ import os
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "sd_server.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sd_server.settings")
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
