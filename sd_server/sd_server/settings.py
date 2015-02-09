56fb885d9da78cb4e1aac776eac8cbda28dc919c
woetzens <nils.woetzel@h-its.org>
2015-02-09 17:08:49 +0100

    added usage of django-configurations values framework http://django-configurations.readthedocs.org/en/latest/values/ for validating settings added some initial stettings for handling qsub user and proxy-user in teh settings.py Default class

--- sd_server/sd_server/settings.py
+++ sd_server/sd_server/settings.py
@@ -2,4 +2,15 @@
 from settings_dev import Dev
 
 class Default(Dev):
+    PBS_USER = ''
+    PBS_PROXY_USER = ''
+    
+    @property
+    def QSUB_FLAGS(self):
+        flags = ""
+        if not self.PBS_USER.empty():
+            flags += " -u " + self.PBS_USER
+        if not self.PBS_PROXY_USER.empty():
+            flags += " -P " + self.PBS_PROXY_USER
+        return flags
     pass
--- sd_server/sd_server/settings_base.py
+++ sd_server/sd_server/settings_base.py
@@ -1,19 +1,19 @@
 # Django settings for sd_server project.
-from configurations import Configuration
+from configurations import Configuration, values
 
 import os
 
 class Base(Configuration):
-    PROJECT_DIR = os.path.abspath(os.path.join( os.path.dirname(__file__),".."))
+    PROJECT_DIR = values.PathValue( os.path.abspath(os.path.join( os.path.dirname(__file__),"..")))
     # Local time zone for this installation. Choices can be found here:
     # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
     # although not all choices may be available on all operating systems.
     # In a Windows environment this must be set to your system time zone.
-    TIME_ZONE = 'Europe/Berlin'
+    TIME_ZONE = values.Value('Europe/Berlin')
     
     # Language code for this installation. All choices can be found here:
     # http://www.i18nguy.com/unicode/language-identifiers.html
-    LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = values.Value('en-us')
     
     ADMINS = (
         # ('Your Name', 'your_email@example.com'),
@@ -24,7 +24,7 @@ class Base(Configuration):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': os.path.join(PROJECT_DIR,'sqlite.db'), # Or path to database file if using sqlite3.
+            'NAME': values.PathValue( os.path.join(PROJECT_DIR,'sqlite.db')), # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': '',
             'PASSWORD': '',
@@ -37,37 +37,37 @@ class Base(Configuration):
     # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
     ALLOWED_HOSTS = ['*']
     
-    SITE_ID = 1
+    SITE_ID = values.IntegerValue(1)
     
     # If you set this to False, Django will make some optimizations so as not
     # to load the internationalization machinery.
-    USE_I18N = True
+    USE_I18N = values.BooleanValue(True)
     
     # If you set this to False, Django will not format dates, numbers and
     # calendars according to the current locale.
-    USE_L10N = True
+    USE_L10N = values.BooleanValue(True)
     
     # If you set this to False, Django will not use timezone-aware datetimes.
-    USE_TZ = True
+    USE_TZ = values.BooleanValue(True)
     
     # Absolute filesystem path to the directory that will hold user-uploaded files.
     # Example: "/var/www/example.com/media/"
-    MEDIA_ROOT = os.path.join(PROJECT_DIR,'upload','')
+    MEDIA_ROOT = values.PathValue( os.path.join(PROJECT_DIR,'upload',''))
     
     # URL that handles the media served from MEDIA_ROOT. Make sure to use a
     # trailing slash.
     # Examples: "http://example.com/media/", "http://media.example.com/"
-    MEDIA_URL = '/download/'
+    MEDIA_URL = values.Value('/download/')
     
     # Absolute path to the directory static files should be collected to.
     # Don't put anything in this directory yourself; store your static files
     # in apps' "static/" subdirectories and in STATICFILES_DIRS.
     # Example: "/var/www/example.com/static/"
-    STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
+    STATIC_ROOT = values.PathValue( os.path.join(PROJECT_DIR,'static'))
     
     # URL prefix for static files.
     # Example: "http://example.com/static/", "http://static.example.com/"
-    STATIC_URL = '/static/'
+    STATIC_URL = values.Value('/static/')
     
     # Additional locations of static files
     STATICFILES_DIRS = (
@@ -79,23 +79,23 @@ class Base(Configuration):
     
     # List of finder classes that know how to find static files in
     # various locations.
-    STATICFILES_FINDERS = (
+    STATICFILES_FINDERS = values.BackendsValue([
         'django.contrib.staticfiles.finders.FileSystemFinder',
         'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #        'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    )
+    ])
     
     # Make this unique, and don't share it with anybody.
     SECRET_KEY = 'w8pe*1leyn!_so&^299$2%v7n48l%$r=)951cv)539b=uw5egu'
     
     # List of callables that know how to import templates from various sources.
-    TEMPLATE_LOADERS = (
+    TEMPLATE_LOADERS = values.BackendsValue([
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
     #     'django.template.loaders.eggs.Loader',
-    )
+    ])
     
-    MIDDLEWARE_CLASSES = (
+    MIDDLEWARE_CLASSES = values.BackendsValue([
         'django.middleware.common.CommonMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
@@ -103,9 +103,9 @@ class Base(Configuration):
         'django.contrib.messages.middleware.MessageMiddleware',
         # Uncomment the next line for simple clickjacking protection:
         # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    )
+    ])
     
-    ROOT_URLCONF = 'sd_server.urls'
+    ROOT_URLCONF = values.Value( 'sd_server.urls')
     
     # Python dotted path to the WSGI application used by Django's runserver.
     WSGI_APPLICATION = 'sd_server.wsgi.application'
@@ -117,7 +117,7 @@ class Base(Configuration):
         os.path.join(PROJECT_DIR,'templates')
     )
     
-    INSTALLED_APPS = (
+    INSTALLED_APPS = values.BackendsValue([
         'django.contrib.auth',
         'django.contrib.contenttypes',
         'django.contrib.sessions',
@@ -130,7 +130,7 @@ class Base(Configuration):
         # 'django.contrib.admindocs',
         'ptp',
         'gmyc',
-    )
+    ])
     
     # A sample logging configuration. The only tangible logging
     # performed by this configuration is to send an email to
