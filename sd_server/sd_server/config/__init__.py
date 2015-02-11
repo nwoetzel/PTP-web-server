36956d849952e6154958181c4edb2da660c7f77e
woetzens <nils.woetzel@h-its.org>
2015-02-11 09:35:22 +0100

    added empty server configuration, that should be overwritten for the Deployment

--- sd_server/sd_server/config/__init__.py
+++ sd_server/sd_server/config/__init__.py
@@ -5,7 +5,4 @@ from __future__ import absolute_import
 
 from .develoment import Development
 from .production import Production
-try:
-    from .server import Server
-except:
-    pass
+from .server import Server
---
+++ sd_server/sd_server/config/server.py
@@ -0,0 +1,5 @@
+# Django settings for sd_server project.
+from .production import Production
+
+class Server(Production):
+    pass
